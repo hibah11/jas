b0212837f5962475e9c322a99ead1d4df87e14e1
hibah11 <hboba-2012@hotmail.com>
2023-08-07 16:46:45 +0300

    all lab qs

--- main.js
+++ main.js
@@ -1,3 +1,55 @@
+
+let number = 4
+if(number % 2 == 0){
+console.log("The number is even");
+}
+else {
+    console.log("The number is odd");
+}
+
+/**
+ * 
+ */
+
+
+
+let grade = 95
+
+if(grade >= 90){
+console.log("your grade is A");
+}
+
+else if (grade >=80 && grade <90 ) {
+    console.log("your grade is B");
+} 
+
+else if(grade <= 79 && grade >= 70 ){
+console.log("your grade is C");
+}
+
+else if(grade <= 69 && grade >= 60){
+console.log("your grade is D");
+}
+
+else if(grade <= 59){
+    console.log("your grade is F");
+}
+
+
+
+let input = 15
+    if (input % 3 === 0 && input % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (input % 3 === 0) {
+      console.log("Fizz");
+    } else if (input % 5 === 0) {
+      console.log("Buzz");
+    }
+
+
+
+
+
 let sum = 0
 for( let i = 1; i<= 10 ; i++){
  sum = Math.pow(i, 2)
